c3197fa257615eab7b349d0d7259ad9a631211c7
Matthew Brennecke <mbrennecke10@gmail.com>
2018-11-01 10:46:11 -0600

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,10 @@
 var React = require('react');
 
 var Examples = (props) => {
-	<h3>Examples Component</h3>	
+	<div>
+		<h3>Examples Component</h3>
+		<p>This is an example page</p>
+	</div>
 }
 
 module.exports = Examples;
