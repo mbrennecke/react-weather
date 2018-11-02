d2d693ce3089ff2b32f52eb13f452451da264eed
Matthew Brennecke <mbrennecke10@gmail.com>
2018-11-02 10:43:57 -0600

    Style nav bar

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
 
 var Examples = (props) => {
+	return (
 	<div>
 		<h3>Examples</h3>
 		<p>This is an example page</p>
 	</div>
+	)
 }
 
 module.exports = Examples;
