3cc2acf1ff32dadf4dcd1a28fe3b1011bc5e32e3
Matthew Brennecke <mbrennecke10@gmail.com>
2018-11-02 13:54:37 -0600

    weather pages styled

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass ({
 		return (
 			<form onSubmit={this.onFormSubmit}>
 				<div><input type="text" placeholder="Enter a city name" ref="location"/></div>
-				<div><button>Get Weather</button></div>
+				<div><button className="button expanded hollow">Get Weather</button></div>
 			</form>
 		);
 	}
