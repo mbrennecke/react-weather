ea79903e0ebcd01dc4e9da8bc6b91e45745a8dae
Matthew Brennecke <mbrennecke10@gmail.com>
2018-11-02 13:37:07 -0600

    https works now

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,11 +5,7 @@ var About = (props) => {
 	return (
 		<div>
 			<h1 className="center-text">About</h1>
-			<p>I am full stack developer, I enjoy the creativity of front end and the logic of backend design. I bring strong HTML, CSS, and Javascript, jQuery, node.js, element, and React knowledge, leveraged to make complex back end for clean front end applications.</p>
-
-			<p>I have a Bachelor’s Degree in Computer Information Systems, with a minor in Business Informatics, as well as several technical certifications. When challenged with a stressful environment I am able to tap deep reserves and rise above.</p>
-
-			<p>My years in technical support have given me a strong troubleshooting ability and determined tenacity that I am able to bring to bear on bug fixing and problem solving when developing an application.</p>
+			<p>This is a weather application built on React. I have built this to keep up my skills with React, post bootcamp.</p>
 			<ul>
 				<li>
 					<a rel="noopener noreferrer" href="https://mbrennecke.github.io/" target="_blank">My portfolio</a>
